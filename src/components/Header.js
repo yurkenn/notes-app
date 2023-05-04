0142469e4deb5359f06bc7510bfc088793b05c22
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2023-05-04 22:15:10 +0300

    Create Header

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import { Text } from '@chakra-ui/react';
+import React from 'react';
+
+const Header = () => {
+  return (
+    <Text fontSize="2xl" color="white" fontWeight="500" align="center">
+      NotesApp
+    </Text>
+  );
+};
+
+export default Header;
